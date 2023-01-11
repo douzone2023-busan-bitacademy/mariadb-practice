e2e39b67644fc245e2e68c66ab3190b0ccd1fa18
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-01-11 14:52:17 +0900

    jdbc #2

---
+++ jdbc-practice/src/main/java/driver/DriverTest.java
@@ -0,0 +1,36 @@
+package driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverTest {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		
+		try {
+			//1. JDBC Driver Class 로딩
+			Class.forName("driver.MyDriver");
+			
+			//2. 연결하기
+			String url = "jdbc:mydb://127.0.0.1:2204/webdb";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+			
+			System.out.println("연결 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ jdbc-practice/src/main/java/test/DeptVo.java
@@ -0,0 +1,23 @@
+package test;
+
+public class DeptVo {
+	private Long no;
+	private String name;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "DeptVo [no=" + no + ", name=" + name + "]";
+	}
+}
