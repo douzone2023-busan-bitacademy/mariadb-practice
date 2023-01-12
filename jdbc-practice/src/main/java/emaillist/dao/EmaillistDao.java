9c98fc4eae2ac0da9f1dad32d94ba544d094f4e1
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-01-12 14:54:46 +0900

    emailist #1

---
+++ jdbc-practice/src/main/java/emaillist/dao/EmaillistDao.java
@@ -0,0 +1,16 @@
+package emaillist.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import emaillist.vo.EmaillistVo;
+
+public class EmaillistDao {
+
+	public List<EmaillistVo> findAll() {
+		List<EmaillistVo> result = new ArrayList<>();
+		
+		return result;
+	}
+
+}
---
+++ jdbc-practice/src/main/java/emaillist/dao/test/EmaillistDaoTest.java
@@ -0,0 +1,31 @@
+package emaillist.dao.test;
+
+import java.util.List;
+
+import emaillist.dao.EmaillistDao;
+import emaillist.vo.EmaillistVo;
+
+public class EmaillistDaoTest {
+
+	public static void main(String[] args) {
+		testInsert();
+		testFindAll();
+	}
+
+	private static void testFindAll() {
+		List<EmaillistVo> list = new EmaillistDao().findAll();
+		for(EmaillistVo vo : list) {
+			System.out.println(vo);
+		}
+	}
+
+	private static void testInsert() {
+		EmaillistVo vo = new EmaillistVo();
+		vo.setFirstName("둘");
+		vo.setLastName("리");
+		vo.setEmail("dooly@gmail.com");
+		
+		new EmaillistDao().insert(vo);
+	}
+
+}
