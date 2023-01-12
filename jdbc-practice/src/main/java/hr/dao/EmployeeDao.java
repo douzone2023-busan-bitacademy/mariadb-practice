b141d8c617b6ec83b5e4dc58594e2bf49655affc
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-01-12 13:57:36 +0900

    hr #1

---
+++ jdbc-practice/src/main/java/hr/dao/EmployeeDao.java
@@ -0,0 +1,78 @@
+package hr.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import hr.vo.EmployeeVo;
+
+public class EmployeeDao {
+	public List<EmployeeVo> findByName(String keyword) {
+		List<EmployeeVo> result = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			String url = "jdbc:mariadb://192.168.10.125:3307/employees?charset=utf8";
+			conn = DriverManager.getConnection(url, "hr", "hr");
+			
+			String sql =
+				"select emp_no, first_name, last_name, date_format(hire_date, '%Y-%m-%d')" + 
+				"  from employees" +
+				" where first_name like ?" + 
+				"    or last_name like ?";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, "%" + keyword + "%");
+			pstmt.setString(2, "%" + keyword + "%");
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				String hireDate = rs.getString(4);
+
+				EmployeeVo vo = new EmployeeVo();
+				vo.setNo(no);
+				vo.setFirstName(firstName);
+				vo.setLastName(lastName);
+				vo.setHireDate(hireDate);
+				
+				result.add(vo);
+			}
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				
+				if(conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return result;
+	}
+}
---
+++ jdbc-practice/src/main/java/hr/vo/EmployeeVo.java
@@ -0,0 +1,51 @@
+package hr.vo;
+
+public class EmployeeVo {
+	private Long no;
+	private String birthDate;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String hireDate;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(String hireDate) {
+		this.hireDate = hireDate;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeVo [no=" + no + ", birthDate=" + birthDate + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", gender=" + gender + ", hireDate=" + hireDate + "]";
+	}
+}
