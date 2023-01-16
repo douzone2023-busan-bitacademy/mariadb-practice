4d83c7cb5a17df897413948098bd6c20b0d935c3
douzone2023-busan-bitacademy <douzone2023.busan.bitacademy@gmail.com>
2023-01-16 10:10:23 +0900

    .

---
+++ jdbc-practice/src/main/java/bookmall/main/BookMall.java
@@ -0,0 +1,25 @@
+package bookmall.main;
+
+public class BookMall {
+
+	public static void main(String[] args) {
+
+		System.out.println("## 회원리스트");
+
+		
+		System.out.println("## 카테고리");
+
+		
+		System.out.println("## 상품");
+		
+		
+		System.out.println("## 카트");
+
+		
+		System.out.println("## 주문");
+
+		System.out.println("## 주문 도서 리스트");
+
+	}
+
+}
